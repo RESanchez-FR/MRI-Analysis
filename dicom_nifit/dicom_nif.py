d4c12c5a61b7847cfda2b54b6736d133de6aeac0
Ramon Sanchez <ramonsan124@gmail.com>
2025-03-26 10:48:08 -0700

    new branch and test

--- dicom_nifit/dicom_nif.py
+++ dicom_nifit/dicom_nif.py
@@ -1,52 +1,11 @@
-# import os
-# import pydicom
-# import nibabel as nib
-# import numpy as np
-# from glob import glob
-
-
-# def dicom_to_nifti(dicom_dir, output_file):
-#     # Find all DICOM files in the directory
-#     dicom_files = glob(os.path.join(dicom_dir, '*.dcm'))
-    
-#     # Sort the files to ensure correct order
-#     dicom_files.sort()
-    
-#     # Read the first DICOM file to get metadata
-#     first_slice = pydicom.dcmread(dicom_files[0])
-    
-#     # Extract necessary metadata
-#     pixel_spacing = first_slice.PixelSpacing
-#     slice_thickness = first_slice.SliceThickness
-    
-#     # Read all DICOM files and stack them into a 3D array
-#     slices = [pydicom.dcmread(dcm) for dcm in dicom_files]
-#     image_3d = np.stack([s.pixel_array for s in slices])
-    
-#     # Create affine matrix
-#     affine = np.eye(4)
-#     affine[0, 0] = pixel_spacing[0]
-#     affine[1, 1] = pixel_spacing[1]
-#     affine[2, 2] = slice_thickness
-    
-#     # Create NIfTI image
-#     nifti_image = nib.Nifti1Image(image_3d, affine)
-    
-#     # Save NIfTI file
-#     nib.save(nifti_image, output_file)
-
-# path = os.getcwd()
-# # Example usage
-# dicom_folder = path + "\\dicom_images\\AL\\DTI_30_AL_ColFA"
-# output_nifti = path + "\\Nii_files\\AL_DTI_COLFA.nii.gz"
-# dicom_to_nifti(dicom_folder, output_nifti)
-
 
 import os
 from glob import glob
 import pydicom
 import numpy as np
 import nibabel as nib
+
+"Create a folder path where the dicom images can be read "
 
 def dicom_to_nifti(dicom_dir, output_file):
     # Find all DICOM files in the directory
@@ -87,6 +46,7 @@ def dicom_to_nifti(dicom_dir, output_file):
 
 # Keep the original input and output sections
 path = os.getcwd()
+
 # Example usage
 dicom_folder = path + "\\dicom_images\\KJ\\DTI_30_dir50pFOVonlyb0sagPAmCS_16"
 output_nifti = path + "\\Nii_files\\KJ_PA.nii.gz"
